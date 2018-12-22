cc0fe3543a93e9f33086577e6d1d25f10b753fae
hai <haithngn@gmail.com>
2018-12-22 16:24:16 +0700

    - add binary framework

---
+++ DCFiOS/Carthage/Build/iOS/CoreFlo.framework/Headers/CoreFlo.h
@@ -0,0 +1,19 @@
+//
+//  CoreFlo.h
+//  CoreFlo
+//
+//  Created by Hai Nguyen on 6/27/18.
+//  Copyright © 2018 Left Coast Logic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreFlo.
+FOUNDATION_EXPORT double CoreFloVersionNumber;
+
+//! Project version string for CoreFlo.
+FOUNDATION_EXPORT const unsigned char CoreFloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreFlo/PublicHeader.h>
+
+
