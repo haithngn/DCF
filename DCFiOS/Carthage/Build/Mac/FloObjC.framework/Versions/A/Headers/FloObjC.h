92d264fc155b3f200683043f4a186ac145937bbc
hai <hai@leftcoastlogic.com>
2018-12-27 09:59:56 +0700

    - add macos framework bin files

---
+++ DCFiOS/Carthage/Build/Mac/FloObjC.framework/Versions/A/Headers/FloObjC.h
@@ -0,0 +1,19 @@
+//
+//  FloObjC.h
+//  FloObjC
+//
+//  Created by Hai Nguyen on 12/13/18.
+//  Copyright © 2018 Left Coast Logic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloObjC.
+FOUNDATION_EXPORT double FloObjCVersionNumber;
+
+//! Project version string for FloObjC.
+FOUNDATION_EXPORT const unsigned char FloObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloObjC/PublicHeader.h>
+
+
