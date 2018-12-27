97aa765ef06854c94109aaa05a415840ae60c7d1
hai <hai@leftcoastlogic.com>
2018-12-27 16:04:29 +0700

    - correct test script (project location)

--- DCFiOS/tests.sh
+++ DCFiOS/tests.sh
@@ -2,9 +2,9 @@ report="../build/tests/tests.log"
 scheme="DCFiOS"
 testscheme="TestServices"
 device_instance="iphonesimulator11.4"
-xcodebuild -project DCFiOS.xcodeproj -scheme $scheme -sdk $device_instance build-for-testing
+xcodebuild -project DCFiOS/DCFiOS.xcodeproj -scheme $scheme -sdk $device_instance build-for-testing
 echo "testing..."
-xctool -project DCFiOS.xcodeproj -scheme $testscheme -launch-timeout 3600 -sdk $device_instance run-tests -reporter plain:$report
+xctool -project DCFiOS/DCFiOS.xcodeproj -scheme $testscheme -launch-timeout 3600 -sdk $device_instance run-tests -reporter plain:$report
 echo $report
 cat $report
 curl -F file=@"$report" -F title="DCFiOS_Unit_Testing_Report.txt" -F filename="$report" -F channels=#tests -F token=xoxp-186550275043-187447070119-255877486165-8a38504cc070664b644cbb957ce69dd9 https://slack.com/api/files.upload
