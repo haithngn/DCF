26be8251788653e27ca1fd79fe483a23ea47d7f6
hai <hai@leftcoastlogic.com>
2018-12-28 18:17:17 +0700

    add precompile frameworks

---
+++ ReactFlo/ios/Carthage/Build/iOS/FloRest.framework/Headers/FloRest.h
@@ -0,0 +1,19 @@
+//
+//  FloRest.h
+//  FloRest
+//
+//  Created by Hai Nguyen on 12/11/18.
+//  Copyright © 2018 Left Coast Logic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloRest.
+FOUNDATION_EXPORT double FloRestVersionNumber;
+
+//! Project version string for FloRest.
+FOUNDATION_EXPORT const unsigned char FloRestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloRest/PublicHeader.h>
+
+
